7f64b7657cf6b7827262c068c00032dfb1cb0b87
Nikhil Reddy <podduturi.nikhil@gmail.com>
2016-09-03 12:01:55 +0200

    Update to code

--- web/app.py
+++ web/app.py
@@ -1,8 +1,11 @@
-import time
+import json
 import logging
+import os
+import requests
+import time
 
 from config import BaseConfig
-from flask import Flask, render_template, g, jsonify
+from flask import Flask, render_template, g, jsonify, request
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,10 +18,57 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 @cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
 def index():
     return render_template("index.html")
+
+
+@app.route('/element/<name>', methods=['GET'])
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
+def elements(name=None):
+    pollutant = name.upper()
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "data", "element.json")
+    data = json.load(open(json_url))
+    results = {}
+    for element in data['elements']:
+        if element['symbol'] == pollutant:
+            results = element
+    if request.args.get('format', None) == 'json':
+        return jsonify(**results)
+    return render_template("element.html", results=results)
+
+
+@app.route('/measurements', methods=['GET'])
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
+def measurements():
+    coordinates = request.args.get('coordinates')
+    r = requests.get(
+        "https://api.openaq.org/v1/measurements?limit=1&coordinates=" + coordinates)
+    results = r.json()
+    return jsonify(results)
+
+
+@app.route('/locations', methods=['GET'])
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
+def locations():
+    r = requests.get("https://api.openaq.org/v1/locations")
+    results = r.json()
+    data = {'results': []}
+    for result in results['results']:
+        if 'coordinates' not in result:
+            continue
+        if result['coordinates']['latitude'] is None:
+            continue
+        if result['coordinates']['longitude'] is None:
+            continue
+        result['lat'] = result['coordinates']['latitude']
+        result['lon'] = result['coordinates']['longitude']
+        data['results'].append(result)
+    if request.args.get('format', None) == 'json':
+        return jsonify(data)
+    return render_template("locations.html", data=data)
 
 
 @app.before_first_request
